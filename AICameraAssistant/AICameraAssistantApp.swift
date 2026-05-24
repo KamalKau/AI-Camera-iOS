8d596b6b0fc58e9a7e5ddc7e6b7addf2909a1073
KamalKau <68215661+KamalKau@users.noreply.github.com>
2026-05-23 23:57:01 -0400

    Initial Commit

---
+++ AICameraAssistant/AICameraAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AICameraAssistantApp.swift
+//  AICameraAssistant
+//
+//  Created by Jesy Multani on 2026-05-23.
+//
+
+import SwiftUI
+
+@main
+struct AICameraAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AICameraAssistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AICameraAssistant
+//
+//  Created by Jesy Multani on 2026-05-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
